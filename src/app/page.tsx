b7b34c30b8e8ed0d9b607669fb8fd7792dd33bd3
Shivanktyagi07 <shivanktyagi976@gmail.com>
2024-03-04 21:16:44 +0530

    hero-section complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import HeroSection from "./components/HeroSection";
 
 
 export default function Home() {
   return(
-    <>
-        <h1 className = "text-bg text-center">chai aur code</h1>
-    </>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <HeroSection />
+    
+  </main>
   )
 }
